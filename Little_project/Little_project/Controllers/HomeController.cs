df85148e9f2e087a43305791922934b1eab2c2e7
Nastiona Kruglaya <ariaignis.dragon@gmail.com>
2021-10-04 15:52:36 +0300

    Invitation site

---
+++ Little_project/Little_project/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Little_project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Little_project.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Response.Where(r => r.willAttend == true));
+        }
+    }
+}
---
+++ Little_project/Little_project/Models/Repository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Little_project.Models
+{
+    public class Repository
+    {
+        private static List<GuestResponse> responses =
+            new List<GuestResponse>();
+        public static IEnumerable<GuestResponse> Response
+        {
+            get
+            {
+                return responses;
+            }
+        }
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
